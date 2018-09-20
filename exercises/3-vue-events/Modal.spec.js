a412e8caa577e851946ea69af90a8a606398f1ef
Imogen Hardy <imogen.hardy@gmail.com>
2018-09-20 13:54:26 +0100

    Fourth and fifth exercises

--- exercises/3-vue-events/Modal.spec.js
+++ exercises/3-vue-events/Modal.spec.js
@@ -4,6 +4,7 @@ import Modal from "./Modal.vue";
 test("emits on-close event when close button is clicked", () => {
   const wrapper = shallowMount(Modal);
 
-  wrapper.find("button").trigger("click");
+  expect(wrapper.emitted("close-modal")).toBeUndefined();
+  wrapper.find("[aria-label='close']").trigger("click");
   expect(wrapper.emitted("close-modal")).toBeTruthy();
 });
--- exercises/4-stubbing-components/Menu.spec.js
+++ exercises/4-stubbing-components/Menu.spec.js
@@ -1,3 +1,29 @@
-test('renders router-link to /login page when signedIn is false')
+import { RouterLinkStub, shallowMount } from "@vue/test-utils";
+import Menu from "./Menu.vue";
 
-test('renders <a> that signs user out if signedIn is true')
+test("renders router-link to /login page when signedIn is false", () => {
+  const wrapper = shallowMount(Menu, {
+    stubs: {
+      "router-link": RouterLinkStub
+    },
+    propsData: {
+      signedIn: false
+    }
+  });
+  expect(wrapper.find(RouterLinkStub).props("to")).toBe("/login");
+});
+
+test("renders <a> that signs user out if signedIn is true", () => {
+  const signOut = jest.fn();
+  const wrapper = shallowMount(Menu, {
+    stubs: {
+      "router-link": RouterLinkStub
+    },
+    propsData: {
+      signedIn: true,
+      signOut
+    }
+  });
+  wrapper.find("a").trigger("click");
+  expect(signOut).toHaveBeenCalled();
+});
